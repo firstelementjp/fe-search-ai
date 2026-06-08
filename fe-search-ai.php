c27a3cca19ba7892793b6a579b579f0e58968290
dxd5001 <dxd5001@gmail.com>
2026-06-08 10:23:23 +0900

    Add License and License URI to plugin header for Plugin Check compliance
    - Add License: GPLv2 or later
    - Add License URI: https://www.gnu.org/licenses/gpl-2.0.html
    - Fixes plugin_header_no_license error

--- fe-search-ai.php
+++ fe-search-ai.php
@@ -6,6 +6,8 @@
  * Version:     0.9.0
  * Author:      FirstElement K.K., Daijiro Miyazawa
  * Author URI:  https://www.firstelement.co.jp/
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: fe-search-ai
  * Domain Path: /languages/
  */
